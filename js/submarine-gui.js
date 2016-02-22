1ec940d69df8ec5360d926a82500bc0170e00d03
Alex Nishikawa <alex.nishikawa@infospace.com>
2016-02-21 21:53:36 -0800

    add speed gauge and click handlers back in

--- js/submarine-gui.js
+++ js/submarine-gui.js
@@ -27,26 +27,80 @@ editor.setShowPrintMargin(false);
 var teacher = Teacher.Create(lessonEditor);
 teacher.nextLesson();
 
+var $playerQueue = $("#playerQueue");
+
 $(function(){
     $("#transmit-button").click(function(){
         teacher.execute(editor.getValue());
     });
-});
 
-var $autoBtn = $('#auto-button');
-var $lessonBtn = $('#lesson-button');
-var $editor = $('#editor-panel');
-var $autoPanel = $('#auto-panel');
-var $transmitPanel = $("#transmit-panel");
-var $playerQueue = $('#playerQueue');
+    var $autoBtn = $('#auto-button');
+    var $lessonBtn = $('#lesson-button');
+    var $lessonPanel = $("#lesson-panel");
+    var $editor = $('#editor-panel');
+    var $autoPanel = $('#auto-panel');
+    var $transmitPanel = $("#transmit-panel");
+    var $subRight = $("#sub-right");
+    var $subLeft = $("#sub-left");
+    var $subForward = $("#sub-forward");
+    var $subBackward = $("#sub-backward");
+    var $subDive = $("#sub-dive");
+    var $subSurface = $("#sub-surface");
 
-$("#auto-button,#lesson-button").mousedown( function() {
-    $autoBtn.toggleClass("pressed");
-    $lessonBtn.toggleClass("pressed");
 
-    $editor.toggleClass("hidden");
-    $autoPanel.toggleClass("hidden")
-    $transmitPanel.toggleClass("hidden");
+    var autoPilot = true;
+    var autoSub = submarineFactory.createSubmarine();
+
+    //CLICK HANDLERS: -----------------------------------------
+
+    $autoBtn.mousedown(function(){
+        autoPilot = true;
+        autoSub.stop();
+
+        $autoBtn.addClass("pressed");
+        $lessonBtn.removeClass("pressed");
+
+        $lessonPanel.css("visibility", "hidden");
+
+        $editor.addClass("hidden");
+        $autoPanel.removeClass("hidden");
+
+        $transmitPanel.addClass("hidden");
+    });
+
+    $lessonBtn.mousedown(function(){
+        autoPilot = false;
+        autoSub.stop();
+
+        $lessonBtn.addClass("pressed");
+        $autoBtn.removeClass("pressed");
+
+        $lessonPanel.css("visibility", "visible");
+
+        $autoPanel.addClass("hidden");
+        $editor.removeClass("hidden");
+
+        $transmitPanel.removeClass("hidden");
+    });
+
+    $subForward.mousedown(function(){ autoSub.moveForward(); });
+    $subForward.mouseup(function(){ autoSub.stop(); });
+
+    $subLeft.mousedown(function(){ autoSub.moveLeft(); });
+    $subLeft.mouseup(function(){ autoSub.stop(); });
+
+    $subRight.mousedown(function(){ autoSub.moveRight(); });
+    $subRight.mouseup(function(){ autoSub.stop(); });
+
+    $subBackward.mousedown(function(){ autoSub.moveBackward(); });
+
+    $subBackward.mouseup(function(){ autoSub.stop(); });
+
+    $subDive.mousedown(function(){ autoSub.moveDown(); });
+    $subDive.mouseup(function(){ autoSub.stop(); });
+
+    $subSurface.mousedown(function(){ autoSub.moveUp(); });
+    $subSurface.mouseup(function(){ autoSub.stop(); });
 });
 
 (function() {
