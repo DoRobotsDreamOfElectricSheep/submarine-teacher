b840b027debe394043df0c3424249f3ee6d6e49a
alexunwin <alex.unwin@infospace.com>
2016-02-21 18:52:38 -0800

    more queue stuff

--- js/submarine-gui.js
+++ js/submarine-gui.js
@@ -1,5 +1,9 @@
 // Setup the WebSocket connection and start the player
 var client = new WebSocket('ws://52.37.12.182:8084/' );
+var endpoint = 'http://52.11.235.138:4000';
+// var endpoint = 'http://localhost:4000';
+var uid = '';
+var username = '';
 var canvas = document.getElementById('vision');
 var player = new jsmpeg(client, {canvas:canvas});
 
@@ -34,6 +38,7 @@ var $lessonBtn = $('#lesson-button');
 var $editor = $('#editor-panel');
 var $autoPanel = $('#auto-panel');
 var $transmitPanel = $("#transmit-panel");
+var $playerQueue = $('#playerQueue');
 
 $("#auto-button,#lesson-button").mousedown( function() {
     $autoBtn.toggleClass("pressed");
@@ -43,3 +48,67 @@ $("#auto-button,#lesson-button").mousedown( function() {
     $autoPanel.toggleClass("hidden")
     $transmitPanel.toggleClass("hidden");
 });
+
+(function() {
+    uid = getGuid();
+    $.post(endpoint + '/user', {uid: uid}, function(resp) {
+        if(resp === null || !resp.length) {
+            return;
+        }
+
+        username = resp;
+        $.post(endpoint + '/userlist', {uid: uid}, function(resp) {
+            console.log(resp.statusCode);
+            updatePlayerQueue(resp.queue);
+            if(resp.statusCode === 43434230) {
+                updatePlayerQueue(resp.queue);
+                startPinging();
+                return;
+            }
+            startPolling();
+        })
+    })
+})();
+
+function updatePlayerQueue(players) {
+    $playerQueue.text('');
+    for (var i in players) {
+        var position = parseInt(i) + 1;
+        if(username === players[i]) {
+            $playerQueue.append(position + '. you</br>');
+        } else {
+            $playerQueue.append(position + '. ' + players[i] + '</br>');
+        }
+    }
+}
+
+function getGuid() {
+    return (
+        'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        })
+    );
+}
+
+function startPinging() {
+    clearInterval(pollId);
+    setInterval(function() {
+        $.post(endpoint + '/ping', {uid: uid}, function(resp) {
+            updatePlayerQueue(resp.queue);
+        })
+    },3000);
+}
+
+var pollId = null;
+function startPolling() {
+    pollId = setInterval(function() {
+        $.post(endpoint + '/userlist', {uid: uid}, function(resp) {
+            if(resp.statusCode === 43434230) {
+                startPinging();
+            }
+            updatePlayerQueue(resp.queue);
+        })
+    },4000);
+}
